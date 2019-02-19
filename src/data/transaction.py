2067e7ea7eef6cf9942f7afc22395e756adfcaad
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-02-19 11:58:44 -0600

    Added new directory for blockchain and added transaction.py

---
+++ src/data/transaction.py
@@ -0,0 +1,7 @@
+"""
+This transaction class set up what the user can put into a transaction
+Currently, there are no functions yet to be implemented
+"""
+class transaction:
+    def __init__(self):
+        pass
