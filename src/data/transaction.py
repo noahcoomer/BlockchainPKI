7c8124c137ca4332ef6fecc7c2b677129104b6c6
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-02-21 12:05:54 -0600

    Deleted getters

--- src/data/transaction.py
+++ src/data/transaction.py
@@ -2,10 +2,16 @@
 This is the setup for our transaction which contains version, transaction_id, transaction_type, tx_generator_address, inputs, outputs
 and I also added some getters to retrieve information from the transaction 
 """
+
+'''
+don't need getters delete them later
+'''
+
+
 class Transaction:
-    
-    #constructor that set up the fields of the transaction 
-    
+
+    # constructor that set up the fields of the transaction
+
     def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs):
         self.version = version
         self.transaction_id = transaction_id
@@ -13,40 +19,3 @@ class Transaction:
         self.tx_generator_address = tx_generator_address
         self.inputs = inputs
         self.outputs = outputs
-
-    
-    # this function returns all the values for the parameters
-    
-    def return_all(self):
-        return "Version", self.version, " transactionID: ", self.transaction_id, "Transaction Type: ", self.transaction_type, " Tx Generator Address: " , self.tx_generator_address, "inputs: ", self.inputs, "outputs: ", self.outputs
-
-    # this function returns the version of the transaction 
-    
-    def return_version(self):
-        return self.version
-    
-    # this function returns the transaction_id for transaction 
-    
-    def return_transaction_id(self):
-        return self.transaction_id
-
-    # this function returns the transaction_type for the transaction     
-    
-    def return_transaction_type(self):
-        return self.transaction_type
-    
-    # this function returns the tx_generator_address for the transaction 
-    
-    def return_tx_Generator_Address(self):
-        return self.tx_generator_address
-    
-    # this function returns the inputs for the transaction 
-    
-    def return_inputs(self):
-        return self.inputs
-
-    # this function returns the outputs for the transaction     
-    
-    def return_Outputs(self):
-        return self.outputs
-
