426ebc57abdc5af9b71f18ef495e8ca94738ee48
dungle <dungle@DELL.localdomain>
2019-02-23 11:05:01 -0600

    Modified block_header and blockchain

--- src/data/blockchain.py
+++ src/data/blockchain.py
@@ -1,21 +1,21 @@
 from block_header import Block
 
+
 class Blockchain:
 
     def __init__(self):
-        self.unconfirm_transactions = [] 
+        self.unconfirm_transactions = []
         self.chain = []
         self.create_genesis_block()
 
-    
     def create_genesis_block(self):
         genesis_block = Block(
-            version = 0, 
-            id = 0, 
-            transactions = [], 
-            previous_hash = "0",
+            version=0,
+            id=0,
+            transactions=[],
+            previous_hash="0",
             merkle_hash="",
-            block_generator_address="", 
+            block_generator_address="",
             block_generation_proof="",
             nonce=0,
             status="confirmed",
@@ -24,12 +24,12 @@ class Blockchain:
 
         self.chain.append(genesis_block)
 
-
     @property
     def last_block(self):
         return self.chain[-1]
 
-    def add_block():
+    # def add_block():
 
     # Validate the hash of the block and some criterias
-    def is_valid_proof():
+    # def is_valid_proof(self):
+    #   return ""
