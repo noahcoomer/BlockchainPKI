22c8b5fe16c4720c29ec261e5f7e15ec3a5b7272
boellis <boellis@memphis.edu>
2019-02-25 15:07:16 -0600

    Updated client functions for generating pairs

--- src/network/client.py
+++ src/network/client.py
@@ -6,3 +6,19 @@
 class Client(object):
     def __init__(self):
         pass
+
+    #Function to generate key pairs
+    def generateKeyPairs():
+        pass
+
+    #Function to validate key pairs
+    def validateKeyPairs():
+        pass
+
+    #Function to create wallet and facilitate login
+    def createWallet():
+        pass
+
+    #Function to send a transaction
+    def sendTransaction():
+        pass
