acf37aed1aaa609fc7a8b424e2c6bc227731897e
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-02-25 14:49:06 -0600

    Added username, public key, and timestamp fields

--- src/data/transaction.py
+++ src/data/transaction.py
@@ -8,7 +8,7 @@ class Transaction:
 
     # constructor that set up the fields of the transaction
 
-    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp):
+    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp, username, public_key, proof):
         self.version = version
         self.transaction_id = transaction_id
         self.transaction_type = transaction_type
@@ -17,3 +17,6 @@ class Transaction:
         self.outputs = outputs
         self.lock_time = lock_time
         self.time_stamp = time_stamp
+        self.username = username
+        self.public_key = public_key
+        self.proof = proof
