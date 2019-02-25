47977ad1987f3902d6b101a2ff5fb562e38a8413
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-02-25 14:41:25 -0600

    added lock_time and time_stamp for transaction class

--- src/data/transaction.py
+++ src/data/transaction.py
@@ -3,19 +3,17 @@ This is the setup for our transaction which contains version, transaction_id, tr
 and I also added some getters to retrieve information from the transaction 
 """
 
-'''
-don't need getters delete them later
-'''
-
 
 class Transaction:
 
     # constructor that set up the fields of the transaction
 
-    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs):
+    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp):
         self.version = version
         self.transaction_id = transaction_id
         self.transaction_type = transaction_type
         self.tx_generator_address = tx_generator_address
         self.inputs = inputs
         self.outputs = outputs
+        self.lock_time = lock_time
+        self.time_stamp = time_stamp
