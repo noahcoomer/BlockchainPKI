312ce98e8ada6e4778c75e46cde9bbf863a316de
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-02-25 15:45:54 -0600

    Added documentation for transaction.py

--- src/data/transaction.py
+++ src/data/transaction.py
@@ -9,14 +9,16 @@ class Transaction:
     # constructor that set up the fields of the transaction
 
     def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp, username, public_key, proof):
-        self.version = version
-        self.transaction_id = transaction_id
-        self.transaction_type = transaction_type
+        self.version = version  # specifies which rules this transaction follows
+        self.transaction_id = transaction_id  # transaction sequence #
+        self.transaction_type = transaction_type  # Admin/Regular
+        # public key of ttransaction generator-Client or Block validators
         self.tx_generator_address = tx_generator_address
-        self.inputs = inputs
-        self.outputs = outputs
+        self.inputs = inputs  # type of services requested
+        self.outputs = outputs  # request result
+        # a unix timestamp or block number-locktime defines the earlier time that a transaction can be added
         self.lock_time = lock_time
-        self.time_stamp = time_stamp
-        self.username = username
-        self.public_key = public_key
-        self.proof = proof
+        self.time_stamp = time_stamp  # transaction generation time
+        self.username = username  # username for the client
+        self.public_key = public_key  # public key of the client
+        self.proof = proof  # proof
