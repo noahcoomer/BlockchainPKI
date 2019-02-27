7f030cbc532650d1d5201c0de83bd73ae1e958b6
boellis <boellis@memphis.edu>
2019-02-27 12:26:43 -0600

    Created functionality for client to generate keys, encrypt private key, and decrypt pubic key

--- src/network/client.py
+++ src/network/client.py
@@ -1,24 +1,37 @@
-"""
-    Generate public-private key pairs
-"""
 
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+#pip install pycryptodome
 
 class Client(object):
     def __init__(self):
         pass
 
-    #Function to generate key pairs
-    def generateKeyPairs():
-        pass
+    def generate_keys():
+        modulus_lenght = 256 * 4
+        private_key = RSA.generate(modulus_lenght, Random.new().read)
+        public_key = private_key.publickey()
+        return private_key, public_key
 
-    #Function to validate key pairs
-    def validateKeyPairs():
-        pass
+    def encrypt_private_key(a_message, private_key):
+        encryptor = PKCS1_OAEP.new(private_key)
+        encrypted_msg = encryptor.encrypt(a_message)
+        encoded_encrypted_msg = base64.b64encode(encrypted_msg)
+        return encoded_encrypted_msg
 
-    #Function to create wallet and facilitate login
-    def createWallet():
-        pass
+    def decrypt_public_key(encoded_encrypted_msg, public_key):
+        encryptor = PKCS1_OAEP.new(public_key)
+        decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+        decoded_decrypted_msg = encryptor.decrypt(decoded_encrypted_msg)
+        return decoded_decrypted_msg
 
-    #Function to send a transaction
-    def sendTransaction():
-        pass
+private_key, public_key = generate_keys()
+
+message = "This will be my test message"
+
+encoded = encrypt_private_key(message, private_key)
+decoded = decrypt_public_key(encoded, public_key)
+
+print(decoded)
