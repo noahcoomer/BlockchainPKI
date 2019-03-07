4114b56359e36f54cee6de80b573a4a4b0e8c77f
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-03-07 16:50:18 -0600

    Added changes to transaction

--- src/data/transaction.py
+++ src/data/transaction.py
@@ -3,6 +3,15 @@ This is the setup for our transaction which contains version, transaction_id, tr
  
 """
 
+'''
+need to create two different transaction classes one for admin and one for regular  
+email jamal to ask him about what admin_tx and regular_tx and about the fields they take in . Will the admin_tx or regular_tx be hash. Would these transactions have private key. What does outputs request result for 
+outputs do we specify the condition to verify the transaction 
+we need to have a speecial transaction or the beginning transaction that occurs in the block, and it doesn't have any inputs 
+do we need previous transaction hash???
+
+'''
+
 
 class Transaction:
 
@@ -22,3 +31,31 @@ class Transaction:
         self.username = username  # username for the client
         self.public_key = public_key  # public key of the client
         self.proof = proof  # proof
+
+
+'''
+this class defines the admin transaction. this class inherits from Transaction 
+'''
+
+
+class admin_tx(Transaction):
+    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp, username, public_key, proof, round_change, leader_selection):
+        Transaction.__init__(self, version, transaction_id, transaction_type, tx_generator_address,
+                             inputs, outputs, lock_time, time_stamp, username, public_key, proof)
+        self.round_change = round_change #
+        self.leader_selection = leader_selection
+
+
+'''
+this class defines the regular transaction, this class inherits from Transaction 
+'''
+
+
+class regular_tx(Transaction):
+    def __init__(self, version, transaction_id, transaction_type, tx_generator_address, inputs, outputs, lock_time, time_stamp, username, public_key, proof, registration, query, update, revoke):
+        Transaction.__init__(self, version, transaction_id, transaction_type, tx_generator_address,
+                             inputs, outputs, lock_time, time_stamp, username, public_key, proof)
+        self.registration = registration
+        self.query = query
+        self.update = update
+        self.revoke = revoke
