2a5f4ed11d582ac654381fe69ae23727701b3f0e
Ryan Nguyen <ryanthnguyen@gmail.com>
2019-03-14 12:37:56 -0500

    resolve merge conflict

--- src/network/client.py
+++ src/network/client.py
@@ -7,24 +7,21 @@ import base64
 
 
 class Client(object):
-    def __init__(self):
+    def __init__(self):us
         pass
 
-    @staticmethod
     def generate_keys():
         modulus_lenght = 256 * 4
         private_key = RSA.generate(modulus_lenght, Random.new().read)
         public_key = private_key.publickey()
         return private_key, public_key
 
-    @staticmethod
     def encrypt_private_key(a_message, private_key):
         encryptor = Salsa20.new(private_key)
         encrypted_msg = encryptor.encrypt(a_message)
         encoded_encrypted_msg = base64.b64encode(encrypted_msg)
         return encoded_encrypted_msg
 
-    @staticmethod
     def decrypt_public_key(encoded_encrypted_msg, public_key):
         encryptor = Salsa20.new(public_key)
         decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
