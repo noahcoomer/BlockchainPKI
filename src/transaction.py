9daf5ad5008880880eae28b193b0cfc84c275419
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-04-02 17:14:07 -0500

    updating __str__ function to be more useful

--- src/transaction.py
+++ src/transaction.py
@@ -59,8 +59,11 @@ class Transaction:
         return hash_256
 
     def __eq__(self, other):
-        return hash(self) == hash(other)
+        return self.compute_hash() == other.compute_hash()
 
     def __str__(self):
-        bit_str = str(pickle.dumps(self))
-        return bit_str
+        s = "<Transaction: "
+        for attr, value in self.__dict__.items():
+            s += "%s=%s, " % (attr, value or "None")
+        s += " >"
+        return s
