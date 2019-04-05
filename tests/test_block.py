9fd4fe1be1e42a94c88107dffd7efddbbfbfc40d
Marshal Hayes <17213165+marshalhayes@users.noreply.github.com>
2019-04-05 02:38:58 -0500

    moving test file back to tests/

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,46 +1,52 @@
 import sys
-sys.path.append('../src')
-import pickle
+sys.path.append('../')
+
+from transaction import Transaction
+from validator import Validator
+from block import Block
 from hashlib import sha256
-import block, validator, transaction
+
+import pickle
 
 def new_transaction(input):
-    #block_string = json.dumps("123456")
-    #proof_string = json.dumps("000234567")
+    ''' Every time this function is run the transaction + block hashes will changed because of the
+    # time_stamp variable, which always change
 
-    transactions = transaction.Transaction(
-        version=0, 
-        transaction_type="Regular", 
-        tx_generator_address="123.09.02.23", 
-        inputs = input,
-        outputs= "",
-        lock_time = 12334
+    # For testing purposes => need to comment out the time_stamp var 
+    # in Transaction class + Block class
+
+    '''
+    transactions = Transaction(
+        version=0,
+        transaction_type="Regular",
+        tx_generator_address="123.09.02.23",
+        inputs=input,
+        outputs="",
+        lock_time=12334
     )
-    tx_data = pickle.dumps({"transaction" : transactions.__dict__}, sort_keys=True)
+    tx_data = pickle.dumps(transactions)
 
     return tx_data
 
-def new_block():
-    txs = []
-    for i in range(1,10):
-        tx = pickle.loads(new_transaction(i))
-        tx_hash = sha256((tx.encode())).hexdigest()
-        txs.append(tx_hash)
 
-    bl = block.Block(
-        version=0.1,
-        id=0,
-        transactions=txs,
-        previous_hash="",
-        block_generator_address="123.09.02.24",
-        block_generation_proof="None",
-        nonce=0, 
-        status="Proposed"
-    )
+def new_block():
+
+    vl = Validator()
+
+    for i in range(1, 10):
+        tx = new_transaction(i)
+        vl.add_transaction(tx)
+
+    bl = vl.create_block(0, 9)
+    print("Hashes of each transaction is :")
+    for t in bl.sha256_txs:
+        print(t)
+    print("\nMerkel root of the block is ", bl.merkle_root)
+    print("Hash of the block is ", bl.hash)
 
 
 def main():
-    print("Hello World!")
+    new_block()
 
 
 if __name__ == '__main__':
